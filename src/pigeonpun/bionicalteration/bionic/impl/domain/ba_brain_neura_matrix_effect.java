43766d0154284ce1108dd0e5d50a5e4200506d72
CosmicFishi <lyquocdung099@gmail.com>
2024-07-08 22:05:51 -0500

    New branch, working on new tier, adjust the higher tier bionic sprite

---
+++ src/pigeonpun/bionicalteration/bionic/impl/domain/ba_brain_neura_matrix_effect.java
@@ -0,0 +1,84 @@
+package pigeonpun.bionicalteration.bionic.impl.domain;
+
+import com.fs.starfarer.api.campaign.SpecialItemPlugin;
+import com.fs.starfarer.api.campaign.econ.MarketAPI;
+import com.fs.starfarer.api.characters.MutableCharacterStatsAPI;
+import com.fs.starfarer.api.characters.PersonAPI;
+import com.fs.starfarer.api.combat.MutableShipStatsAPI;
+import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.ui.TooltipMakerAPI;
+import pigeonpun.bionicalteration.ba_limbmanager;
+import pigeonpun.bionicalteration.bionic.ba_bioniceffect;
+import pigeonpun.bionicalteration.bionic.ba_bionicitemplugin;
+
+public class ba_brain_neura_matrix_effect implements ba_bioniceffect {
+    @Override
+    public void setBionicItem(ba_bionicitemplugin bionic) {
+
+    }
+
+    @Override
+    public void displayEffectDescription(TooltipMakerAPI tooltip, PersonAPI person, ba_bionicitemplugin bionic, boolean isItem) {
+
+    }
+
+    @Override
+    public String getShortOnRemoveEffectDescription() {
+        return null;
+    }
+
+    @Override
+    public void applyOfficerEffect(MutableShipStatsAPI stats, ShipAPI.HullSize hullSize, String id) {
+
+    }
+
+    @Override
+    public void unapplyOfficerEffect(MutableShipStatsAPI stats, ShipAPI.HullSize hullSize, String id) {
+
+    }
+
+    @Override
+    public void applyAdminEffect(MutableCharacterStatsAPI stats, String id) {
+
+    }
+
+    @Override
+    public void unapplyAdminEffect(MutableCharacterStatsAPI stats, String id) {
+
+    }
+
+    @Override
+    public void applyEffectAdminMarket(MarketAPI market, String id, float level, ba_bionicitemplugin bionic) {
+
+    }
+
+    @Override
+    public void unapplyEffectAdminMarket(MarketAPI market, String id) {
+
+    }
+
+    @Override
+    public boolean isAdvanceInCombat() {
+        return false;
+    }
+
+    @Override
+    public void advanceInCombat(ShipAPI ship, float amount) {
+
+    }
+
+    @Override
+    public void onRemove(PersonAPI person, ba_limbmanager.ba_limb limb, ba_bionicitemplugin bionic) {
+
+    }
+
+    @Override
+    public void onInstall(PersonAPI person, ba_limbmanager.ba_limb limb, ba_bionicitemplugin bionic) {
+
+    }
+
+    @Override
+    public void renderExtraOnItem(float x, float y, float w, float h, float alphaMult, float glowMult, SpecialItemPlugin.SpecialItemRendererAPI renderer) {
+
+    }
+}
