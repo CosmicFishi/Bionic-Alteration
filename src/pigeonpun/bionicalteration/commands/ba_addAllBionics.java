08b9f43dd2e6b27d2cd1cdb2cad5569bcf292c5f
CosmicFishi <lyquocdung099@gmail.com>
2025-06-01 19:08:44 -0500

    Kinna fixed the contact, need more testing

--- src/pigeonpun/bionicalteration/commands/ba_addAllBionics.java
+++ src/pigeonpun/bionicalteration/commands/ba_addAllBionics.java
@@ -5,6 +5,7 @@ import com.fs.starfarer.api.campaign.CargoAPI;
 import com.fs.starfarer.api.campaign.SpecialItemData;
 import org.jetbrains.annotations.NotNull;
 import org.lazywizard.console.BaseCommand;
+import org.lazywizard.console.CommonStrings;
 import org.lazywizard.console.Console;
 import pigeonpun.bionicalteration.bionic.ba_bionicitemplugin;
 import pigeonpun.bionicalteration.bionic.ba_bionicmanager;
@@ -12,6 +13,10 @@ import pigeonpun.bionicalteration.bionic.ba_bionicmanager;
 public class ba_addAllBionics implements BaseCommand {
     @Override
     public CommandResult runCommand(@NotNull String args, @NotNull BaseCommand.CommandContext context) {
+        if (!context.isInCampaign()) {
+            Console.showMessage(CommonStrings.ERROR_CAMPAIGN_ONLY);
+            return CommandResult.WRONG_CONTEXT;
+        }
         for(ba_bionicitemplugin bionic: ba_bionicmanager.bionicItemMap.values()) {
             SpecialItemData specialItem = new SpecialItemData(bionic.bionicId, null);
             Global.getSector().getPlayerFleet().getCargo().addSpecial(specialItem, 1);
--- src/pigeonpun/bionicalteration/events/ba_ripper_bionic.java
+++ src/pigeonpun/bionicalteration/events/ba_ripper_bionic.java
@@ -3,6 +3,11 @@ package pigeonpun.bionicalteration.events;
 import com.fs.starfarer.api.campaign.*;
 import com.fs.starfarer.api.campaign.econ.MarketAPI;
 import com.fs.starfarer.api.campaign.rules.MemoryAPI;
+import com.fs.starfarer.api.characters.PersonAPI;
+import com.fs.starfarer.api.impl.campaign.ids.Factions;
+import com.fs.starfarer.api.impl.campaign.ids.Ranks;
+import com.fs.starfarer.api.impl.campaign.ids.Tags;
+import com.fs.starfarer.api.impl.campaign.missions.ProcurementMission;
 import com.fs.starfarer.api.impl.campaign.missions.hub.HubMissionWithBarEvent;
 import com.fs.starfarer.api.ui.TooltipMakerAPI;
 import com.fs.starfarer.api.util.Misc;
@@ -17,11 +22,6 @@ public class ba_ripper_bionic extends HubMissionWithBarEvent {
         SELLING,
         COMPLETED
     }
-    protected List<ba_bionicitemplugin> listBionics = new ArrayList<>();
-    protected boolean armsDealer = false;
-    protected int maxTotalBionic;
-    protected int minTotalBionic;
-    protected float costMult;
     protected int cost;
     protected FactionAPI faction;
     protected MarketAPI market;
@@ -32,35 +32,42 @@ public class ba_ripper_bionic extends HubMissionWithBarEvent {
     }
     @Override
     protected boolean create(MarketAPI createdAt, boolean barEvent) {
+        if(barEvent) {
+            setGiverRank(Ranks.CITIZEN);
+            setGiverPost(pickOne(Ranks.POST_SMUGGLER, Ranks.POST_GANGSTER,
+                    Ranks.POST_FENCE, Ranks.POST_CRIMINAL));
+            setGiverImportance(pickImportance());
+            setGiverTags(Tags.CONTACT_UNDERWORLD);
+            setGiverFaction(Factions.PIRATES);
+            findOrCreateGiver(createdAt, false, false);
+        }
+        PersonAPI person = getPerson();
+        if (person == null) return false;
+        MarketAPI market = person.getMarket();
+        if (market == null) return false;
+
+        if (!setPersonMissionRef(person, "$ba_ripperBionic_ref")) {
+            return false;
+        }
+
+        if (barEvent) {
+            setGiverIsPotentialContactOnSuccess(1);
+        }
+        setStartingStage(Stage.SELLING);
+        setStageOnMemoryFlag(Stage.COMPLETED, person, "$ba_ripperBionic_completed");
+        setName("RipperDoc contact acquisition");
         return true;
     }
 
     @Override
-    public void setCurrentStage(Object next, InteractionDialogAPI dialog, Map<String, MemoryAPI> memoryMap) {
-        super.setCurrentStage(next, dialog, memoryMap);
-    }
+    public boolean callEvent(String ruleId, InteractionDialogAPI dialog, List<Misc.Token> params, Map<String, MemoryAPI> memoryMap) {
+        String action = params.get(0).getString(memoryMap);
 
-    @Override
-    protected boolean callAction(String action, String ruleId, InteractionDialogAPI dialog, List<Misc.Token> params, Map<String, MemoryAPI> memoryMap) {
-        if(action.equals("ba_ripperBionic_selectBionics")) {
-            dialog.showCargoPickerDialog("Offers", "Confirm", "Cancel", false, 400f, null, new CargoPickerListener() {
-                @Override
-                public void pickedCargo(CargoAPI cargo) {
-
-                }
-
-                @Override
-                public void cancelledCargoSelection() {
-
-                }
-
-                @Override
-                public void recreateTextPanel(TooltipMakerAPI panel, CargoAPI cargo, CargoStackAPI pickedUp, boolean pickedUpFromSource, CargoAPI combined) {
-                    panel.addPara("testing, AHHHHHHHHHHHHHHH", 10f);
-                }
-            });
+        if(action.equals("ba_addRipperDoc_Contact")) {
+            addPotentialContacts(dialog);
+            endSuccess(dialog, memoryMap);
             return true;
         }
-        return super.callAction(action, ruleId, dialog, params, memoryMap);
+        return super.callEvent(ruleId, dialog, params, memoryMap);
     }
 }
