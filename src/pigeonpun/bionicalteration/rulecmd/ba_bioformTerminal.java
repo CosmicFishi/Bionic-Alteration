74d599dc34e5f7480293ae2e5aa8aeeac2c478a8
CosmicFishi <lyquocdung099@gmail.com>
2025-06-14 21:29:58 -0500

    Finish off the upgrade option

--- src/pigeonpun/bionicalteration/rulecmd/ba_bioformTerminal.java
+++ src/pigeonpun/bionicalteration/rulecmd/ba_bioformTerminal.java
@@ -3,13 +3,16 @@ package pigeonpun.bionicalteration.rulecmd;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.*;
 import com.fs.starfarer.api.campaign.rules.MemoryAPI;
+import com.fs.starfarer.api.impl.campaign.ids.Commodities;
 import com.fs.starfarer.api.impl.campaign.rulecmd.BaseCommandPlugin;
 import com.fs.starfarer.api.util.Misc;
+import org.apache.log4j.Logger;
 
 import java.util.List;
 import java.util.Map;
 
 public class ba_bioformTerminal extends BaseCommandPlugin {
+    Logger log = Global.getLogger(ba_bioformTerminal.class);
     protected CampaignFleetAPI playerFleet;
     protected SectorEntityToken entity;
     protected TextPanelAPI text;
@@ -43,7 +46,26 @@ public class ba_bioformTerminal extends BaseCommandPlugin {
         return true;
     }
     public void checkIfCanUpgrade() {
-
+        int requiredCrew = memory.getInt("$ba_upgrade_crewReq");
+        int requiredMetal = memory.getInt("$ba_upgrade_metalsReq");
+        int requiredMachine = memory.getInt("$ba_upgrade_machineReq");
+        int requiredAlphaCore = memory.getInt("$ba_upgrade_ai_alphaReq");
+        boolean disableUpgrade = true;
+        if(checkCargoAvailable(requiredCrew, "crew") && checkCargoAvailable(requiredMetal, "metals") && checkCargoAvailable(requiredMachine, "heavy_machinery") && checkCargoAvailable(requiredAlphaCore, "alpha_core")) {
+            disableUpgrade = false;
+        }
+        if(disableUpgrade) {
+            dialog.getOptionPanel().setEnabled("ba_bioformUpgradeOpt", false);
+        }
+    }
+    public boolean checkCargoAvailable(int amount, String commoditiesId) {
+        int total = 0;
+        for(CargoStackAPI stack: Global.getSector().getPlayerFleet().getCargo().getStacksCopy()) {
+            if(stack.isCommodityStack() && stack.getCommodityId().equals(commoditiesId)) {
+                total += (int) stack.getSize();
+            }
+        }
+        return total >= amount;
     }
     public void upgrade() {
 
